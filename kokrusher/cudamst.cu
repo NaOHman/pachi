1329b50430f09af4a37c0504daeedd82c7a4616a
Jeffrey Lyman <jeffreytlyman@gmail.com>
2015-11-22 16:12:01 -0600

    Basica Cuda Playout stuff

--- kokrusher/cudamst.cu
+++ kokrusher/cudamst.cu
@@ -25,51 +25,81 @@ extern "C" {
 //}
 //#endif
 
+#define MAX_PLAYS 400
+
 const int N = 16; 
 const int blocksize = 16; 
 
-__global__ void hello(char *a, int *b){
-    a[threadIdx.x] += b[threadIdx.x];
+__global__ void running_every_thread(struct board *b, enum stone color, int[] votes){
+    struct *myBoard  = malloc(sizeof(struct board));
+    board_copy(myBoard, b);
+    //make first move according to blckid and tid
+    //where uct happens 
+    int win = cuda_play_random_game(myBoard, color);
+    //record move
 }
 
-void say_hello(){
-    char a[N] = "Hello \0\0\0\0\0\0";
-    int b[N] = {15, 10, 6, 0, -11, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+__device__ int cuda_play_random_game(struct board *b, enum stone starting_color)
+{
+	int gamelen = MAX_PLAYS - b->moves;
+	enum stone color = starting_color;
+	int passes = is_pass(b->last_move.coord) && b->moves > 0;
 
-    char *ad;
-    int *bd;
-    const int csize = N*sizeof(char);
-    const int isize = N*sizeof(int);
+	while (gamelen-- && passes < 2) {
+		color = stone_other(color);
+		coord_t coord = cuda_play_random_move(b, color);
+		if (unlikely(is_pass(coord))) {
+			passes++;
+		} else {
+			passes = 0;
+		}
+	}
 
-    fprintf(stderr, a);
-
-    cudaMalloc( (void**)&ad, csize ); 
-    cudaMalloc( (void**)&bd, isize ); 
-    cudaMemcpy( ad, a, csize, cudaMemcpyHostToDevice ); 
-    cudaMemcpy( bd, b, isize, cudaMemcpyHostToDevice ); 
-
-    dim3 dimBlock( blocksize, 1 );
-    dim3 dimGrid( 1, 1 );
-    hello<<<dimGrid, dimBlock>>>(ad, bd);
-    cudaMemcpy( a, ad, csize, cudaMemcpyDeviceToHost ); 
-    fprintf(stderr, a);
-    cudaFree( ad );
-    cudaFree( bd );
+	floating_t score = board_fast_score(b);
+	int result = (starting_color == S_WHITE ? score * 2 : - (score * 2));
+	if (b->ps)
+		free(b->ps);
+	return result;
 }
+
+__device coord_t cuda_play_random_move(struct board *b, enum stone color){
+	coord_t coord;
+	if (unlikely(b->flen == 0)) {
+        struct move m = { pass, color };
+        board_play(b, &m);
+        return pass;
+    }
+	int base = fast_random(b->flen), f;
+	for (f = base; f < b->flen; f++)
+		if (cuda_try_random_move(b, color, &coord, f))
+			return coord;
+	for (f = 0; f < base; f++)
+		if (cuda_try_random_move(b, color, &coord, f))
+			return coord;
+}
+
+static inline bool
+cuda_try_random_move(struct board *b, enum stone color, coord_t *coord, int f)
+{
+	*coord = b->f[f];
+	struct move m = { *coord, color };
+	if (unlikely(board_is_one_point_eye(b, *coord, color)) /* bad idea to play into one, usually */
+		|| !board_is_valid_move(b, &m))
+		return false;
+	if (m.coord == *coord) {
+		return likely(board_play_f(b, &m, f) >= 0);
+	} else {
+		*coord = m.coord; // permit modified the coordinate
+		return likely(board_play(b, &m) >= 0);
+	}
+}
+
 
 coord_t *cuda_genmove(struct engine *e, struct board *b, struct time_info *ti, enum stone color, bool pass_all_alive){
-    say_hello();
-    coord_t *coord;
-    struct board b2;
-    board_copy(&b2, b);
-    for (int i=0; i<=19; i++){
-        for (int j=0; j<=19; j++){
-            coord = coord_init(i,j,board_size(&b2));
-            if (board_is_valid_play(&b2, color, *coord)){
-                return coord;
-            }
-        }
-    }
-    *coord = -1;
-    return coord;
+    //create array of possible moves
+    //copy board to device
+    //copy array to device
+    //launch kernel
+    //copy array back
+    //pick winning move
 }
