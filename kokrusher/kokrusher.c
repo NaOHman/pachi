22bec18698089b79e5bba7f81f3bf49022f1a54f
Ari Weiland <math1man@gmail.com>
2015-11-30 13:51:14 -0600

    further elaborated previously defined methods, and laid out the important sim_tree method

--- kokrusher/kokrusher.c
+++ kokrusher/kokrusher.c
@@ -41,7 +41,7 @@ engine_kokrusher_init(char *arg, struct board *b)
 {
     struct engine *e = calloc2(1, sizeof(struct engine));
     e->name = "kokrusher";
-    e->comment = "Macalester's own kokrusher";
+    e->comment = "Macalester's own KoKrusher";
     e->genmove = kokrusher_genmove;
     return e;
 }
@@ -53,12 +53,11 @@ struct tree_node {
 		float* child_q;
 }
 
-float uct(float q, int parent_visits, int child_visits) {
-		// q + c * sqrt(log(parent_visits)/child_visits)
+float uct(float child_q, int parent_visits, int child_visits) {
+		// child_q + c * sqrt(log(parent_visits)/child_visits)
 		// c is an arbitrary predefined constant
 		// need to handle child_visits == 0 also
-		// need to handle possibly illegal moves -> output 0
-		return q;
+		return child_q;
 }
 
 float rouletteScalingFunction(float uct) {
@@ -69,23 +68,54 @@ float rouletteScalingFunction(float uct) {
 		return uct;
 }
 
-
-int rouletteSelect(int parent_visits, float* q_array, int* child_visits_array) {
+int roulette_select(int parent_visits, float* q_array, int* child_visits_array) {
 		float total = 0.0;
-		float* uctArray; // I forget how to init this
+		float* uctArray = malloc(sizeof(float)*n*n); // is that how to init that?
 		for (int i=0; i<n*n; i++) { // iterate over length of array: n^2
-				float temp = uct(q_array[i], parent_visits, child_visits_array[i]);
-				temp = rouletteScalingFunction(temp);
-				total += temp;
-				uctArray[i] = temp;
+				// need to ignore illegal moves here
+				if (child_visits_array[i] > -1) { // use visits == -1 to indicate illegal moves
+						float temp = uct(q_array[i], parent_visits, child_visits_array[i]);
+						temp = rouletteScalingFunction(temp);
+						total += temp;
+						uctArray[i] = temp;
+				} else {
+						uctArray[i] = 0;
+				}
 		}
 		float random = 0.0; // set up random number generation here, assuming from [0,1)
 		float pos = random * total;
 		for (int i=0; i<n*n; i++) {
 				pos -= uctArray[i];
-				if (pos < 0) return i; // this should always return...?
+				if (pos < 0) return i; // this should always return, but just in case...
 		}
 		return n*n-1; // in case of emergencies, return final entry
 }
 
+struct move get_move(int n) {
+		// this method needs to covert an integer between 0 and n^2 into a pachi board move
+		return;
+}
+
+struct tree_node* sim_tree(int visits, struct tree_node* current, struct board* b) {
+		// TODO: check if game is over
+		// lets assume the current node exists
+		// and that roulette_select only returns legal moves
+		int next = roulette_select(visits, current->child_q, current->child_visits);
+		struct move m = get_move(next);
+		board_play(b, &m);
+		// check if next node exists, if not, create it
+		if ( /* what is this check? default to null? default to 0? */ ) {
+				return sim_tree(current->child_visits[next], current->children[next], b);
+		} else {
+				return new_node(current, next, b);
+		}
+}
+
+struct tree_node* new_node(struct tree_node* parent, int index, struct board* b) {
+		struct tree_node* n = malloc(/* ? */);
+		n->parent = parent;
+		parent->children[index] = n;
+		// mark illegal children of n here
+		return n;
+}
 
